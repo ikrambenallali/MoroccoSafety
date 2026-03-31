692e3731bd61d924ca84a8fd1291873a7217213e
ikrambenallali <ikrambenallali2003@gmail.com>
2026-03-31 22:28:12 +0100

    fixe problem

--- apps/api/src/auth/decorators/user.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const User = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
-    const req = ctx.switchToHttp().getRequest();
-    return data ? req.user[data] : req.user;
-  },
-);
